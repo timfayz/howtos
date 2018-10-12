0ac06258861ae7a6b2c896603c19d90b421b6d5e
Timur Fayzrakhmanov <tim.fayzrakhmanov@gmail.com>
2018-10-12 23:20:52 +0300

    Update drive backup insts

--- drive-backup/readme.sh
+++ drive-backup/readme.sh
@@ -1,22 +1,27 @@
 exit
 
-Recommendations:
-- Clear cache in Telegram, Firefox, Skype 
-- Empty Trash
+# Recommendations:
+# - Clear cache in Telegram, Firefox, Skype
+# - Empty Trash
 
-# backup /
-tar -cpvf root-dir.tar --one-file-system --exclude-from=backup-root-exclusions.txt ./
+# Backup /
+# add -z to compress
+# use `.*` instead / to start back .folders only
+# add `| split -d -b 3900m - /backup.tar.` to split
+# add `--exclude=root.tar` to not backing up ourselves
+sudo tar -cvpf root.tar --one-file-system --exclude=root.tar --exclude-from=exclusions.txt /
 
-# backup /home
-tar -cpvf home-dir.tar --one-file-system --exclude-from=backup-home-exclusions.txt .*
+# Backup using rsync
+rsync -ax --progress --exclude-from=exclusion-shared.txt source/ dest/
 
-# backup /shared
-rsync -ax --progress --exclude-from=exclusion-shared.txt /mnt/shared/ /mnt/hdd/Backup/
-
-# backup MCST/
-tar -cpv --one-file-system MCST --exclude=sandbox | split -d -b 3900m - /mnt/usb/hdd/mcst-back.tar.
-
-# backup entire drive into partition
+# Backup entire drive into partition
 sudo dd if=/dev/sda of=/dev/sdb1 bs=?K conv=noerror,sync status=progress # | gzip -c > path/to/drive/backup.img.gz
 gunzip -c /path/to/drive/backup.img.gz | dd of=/dev/sda
 
+# Backup/Move LVM to new drive
+1. Backup the whole thing: Timeshift, GNOME disk utility, dd or tar
+2. Shrink LVM volumes: shrink LV (+resize2fs or -r) -> PV -> partition
+3. Migrate (/dev/sdb2 is new drive, a is old)
+	1. gdisk on new drive -> mimic layout of old drive
+	2. pvcreate /dev/sdb2
+	3. ... rewrite notes
