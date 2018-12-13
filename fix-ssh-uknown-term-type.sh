657515ff7780c6414628e8ae7d35308782508741
Timur Fayzrakhmanov <tim.fayzrakhmanov@gmail.com>
2018-12-13 12:05:18 +0300

    Restructure repo

--- fix-ssh-uknown-term-type.sh
+++ fix-ssh-uknown-term-type.sh
@@ -1,7 +1,8 @@
+# Problem
 # 'rxvt-unicode-256color': unknown terminal type.
-# Solution:
+
+# Solution
 # export TERM to some compatible terminal
 export TERM='xterm-256color'
 # copy db file to remote server
 scp /usr/share/terminfo/r/rxvt-unicode-256color user@server:.terminfo/r
-# or
--- how-tos/build-ngspice.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sudo xbps-install readline-devel libXaw-devel
-# read INSTALL instruction (tarball version)
